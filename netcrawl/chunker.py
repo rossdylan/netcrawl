64f72b2ba9d65174b8a71a6bc3d4d4c79c28dea4
Ross <rossdylan@csh.rit.edu>
2012-12-08 18:32:45 -0500

    wrote the ip chunker, which splits the internet into /24 ranges

---
+++ netcrawl/chunker.py
@@ -0,0 +1,25 @@
+"""
+Split up the internet into a series of chunks
+"""
+
+from RedisQueue import RedisQueue
+
+class Chunker(object):
+    def __init__(self, redis_host):
+        self.work_queue = RedisQueue(redis_host, "inqueue")
+
+    def run(self):
+        chunk_id = 0
+        for a in range(1, 255):
+            for b in range(1, 255):
+                for c in range(1, 255):
+                    ip_range = "{0}.{1}.{2}.{3}.0/24".format(a, b, c)
+                    print "Sending chunk {0} range: {1}".format(chunk_id,
+                            ip_range)
+                    task = {
+                            "range": ip_range,
+                            "id": chunk_id
+                           }
+                    self.work_queue.put(task)
+                    chunk_id += 1
+
