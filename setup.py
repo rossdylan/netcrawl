448937879986b7188e080ab3a50e94bd6bb42173
Ross <rossdylan@csh.rit.edu>
2012-12-08 15:09:44 -0500

    Added a basic setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='netcrawl',
+    version='0.1.0',
+    description='Crawl the internet',
+    author='Ross Delinger',
+    author_email='rossdylan@csh.rit.edu',
+    install_requires = [
+        'retask',
+    ],
+    zip_safe=False,
+    )
