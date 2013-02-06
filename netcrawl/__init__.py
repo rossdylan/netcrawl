2ab9fd4e6cb38614fa2ab4eff3bfa181cb3b9dc7
Ross Delinger <rossdylan@csh.rit.edu>
2013-02-06 17:52:21 -0500

    Renamed crawler to spider since it is only used to gather intial links, split WebPage out into the mode

--- netcrawl/__init__.py
+++ netcrawl/__init__.py
@@ -1,31 +1,31 @@
 import chunker
 import scanner
 import receiver
-import crawler
-
+import spider
+import sys
 
 def run_scanner():
-    s = scanner.Scanner("localhost")
+    s = scanner.Scanner(sys.argv[1])
     s.run()
 
 
 def run_chunker():
-    c = chunker.Chunker("localhost")
+    c = chunker.Chunker(sys.argv[1])
     c.run()
 
 
 def run_receiver():
-    r = receiver.Receiver("localhost")
+    r = receiver.Receiver(sys.argv[1])
     r.run()
 
 def run_dump():
-    r = receiver.Receiver("localhost")
+    r = receiver.Receiver(sys.argv[1])
     r.run_dump()
 
 def run_test():
-    c = chunker.Chunker("localhost")
+    c = chunker.Chunker(sys.argv[1])
     c.run_test()
 
-def run_crawler():
-    cr = crawler.Crawler("localhost")
-    cr.run()
+def run_spider():
+    sp = spider.Spider(sys.argv[1])
+    sp.run()
--- netcrawl/spider.py
+++ netcrawl/spider.py
@@ -1,50 +1,9 @@
 from RedisQueue import RedisQueue
-from bs4 import BeautifulSoup
-from pprint import pformat
 import requests
+from model import WebPage
 
 
-class WebPage(object):
-    def __init__(self, html, ip, port):
-        self.ip = ip
-        self.port = port
-        self.html = html
-        soup = BeautifulSoup(self.html)
-        a_tags = soup.find_all('a')
-
-        all_links = map(lambda l: l.get('href'), a_tags)
-        all_links = filter(lambda l: l != None, all_links)
-        relative_links = filter(lambda l: l.startswith("/"), all_links)
-        other_thinks = filter(lambda l: not l.startswith("/"), all_links)
-        self.links = other_thinks
-
-        self.links.extend(map(
-                lambda l: "{0}:{1}{2}".format(
-                    self.ip,
-                    self.port,
-                    l),
-                relative_links))
-        print "Scraped {0} links from {1}:{2}".format(
-                len(self.links),
-                self.ip,
-                self.port)
-
-    def to_dict(self):
-        return {
-                'ip': self.ip,
-                'port': self.port,
-                'html': self.html,
-                'links': self.links
-               }
-
-    def __repr__(self):
-        return "{0}:{1}\n{2}".format(
-                self.ip,
-                self.port,
-                pformat(self.links))
-
-
-class Crawler(object):
+class Spider(object):
     def __init__(self, redis_host):
         self.output_queue = RedisQueue(redis_host, "outqueue")
         self.pages_queue = RedisQueue(redis_host, "pagesqueue")
